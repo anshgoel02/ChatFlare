bbfd223208d67d9dcbd45b19cad1ec810b7ce4d4
Ansh Goel <goelansh164@gmail.com>
2024-07-21 19:19:12 +0530

    Removed secret

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const messageRoutes = require("./routes/messagesRoutes");
 const socket = require("socket.io");
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://goelansh164:mVTOh2jFAGbGYgKC@cluster0.qpup1gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // const client = new MongoClient(uri, {
